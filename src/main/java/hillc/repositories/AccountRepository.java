2757fae1e731b47a7e86e091c506b77f325a71e7
Christopher Hill <ch6574@gmail.com>
2016-04-16 21:31:46 -0400

    Initial add

---
+++ src/main/java/hillc/repositories/AccountRepository.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (C) 2016, Christopher Hill <ch6574@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package hillc.repositories;
+
+import org.example.account.Account;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "account", path = "account")
+public interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
+
+	Page<Account> findByName(@Param("name") String name, Pageable pageable);
+}
